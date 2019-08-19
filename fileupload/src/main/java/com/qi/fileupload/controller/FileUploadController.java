d230c99c02c1680d547de5e4da422fcea3ab5cae
DESKTOP-99JV22L\qixianchuan <qixianchuan@163.com>
2019-08-19 20:28:46 +0800

    fileupload

--- fileupload/src/main/java/com/qi/fileupload/controller/FileUploadController.java
+++ fileupload/src/main/java/com/qi/fileupload/controller/FileUploadController.java
@@ -1,5 +1,8 @@
 package com.qi.fileupload.controller;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.file.FileWriter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,10 +11,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Date;
 
 /**
  * @author ：qixianchuan
@@ -59,28 +64,27 @@ public class FileUploadController {
     }
 
     @PostMapping("/ajaxupload")
-    public @ResponseBody String ajaxFileUpload(@RequestParam("imgFile") MultipartFile imgFile) {
+    @ResponseBody
+    public String upload(@RequestParam("imgFile") MultipartFile imgFile) throws Exception {
         String returnStr = "";
         if (imgFile.isEmpty()) {
             returnStr = "您没有上传文件！";
         }
         try {
             byte[] bytes = imgFile.getBytes();
-            Path path = Paths.get(UPLOADED_FOLDER + imgFile.getOriginalFilename());
+            String now = DateUtil.format(new Date(), "yyyyMMddHHmmss");
+            //为防止图片名称一样产生的覆盖问题,对图片重新命名
+            Path path = Paths.get(UPLOADED_FOLDER + now + imgFile.getOriginalFilename());
+            boolean exist = FileUtil.exist(UPLOADED_FOLDER);
+            if(!exist){
+                FileUtil.mkdir(UPLOADED_FOLDER);
+            }
             Files.write(path, bytes);
             returnStr = "上传成功";
         } catch (IOException e) {
             e.printStackTrace();
         }
         return returnStr;
-//        return "gf";
-    }
-
-
-    public String upload(@RequestParam("imgFile") MultipartFile imgFile) throws Exception{
-        byte[] bytes = imgFile.getBytes();
-
-        return "";
     }
 
     @GetMapping("/uploadStatus")
--- fileupload/src/test/java/com/qi/fileupload/FileuploadApplicationTests.java
+++ fileupload/src/test/java/com/qi/fileupload/FileuploadApplicationTests.java
@@ -1,9 +1,12 @@
 package com.qi.fileupload;
 
+import cn.hutool.core.date.DateUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +14,9 @@ public class FileuploadApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		String s = DateUtil.format(new Date(), "yyyyMMddHHmmssZ");
+		System.out.println("！！！！！！！！！！！！！！！！！！");
+		System.out.println(s);
 	}
 
 }
