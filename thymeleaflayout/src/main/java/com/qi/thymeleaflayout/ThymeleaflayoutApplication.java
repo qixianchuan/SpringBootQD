d947834cd9e74e8645c771a5dc07926f762629af
DESKTOP-99JV22L\qixianchuan <qixianchuan@163.com>
2019-08-23 10:09:54 +0800

    thymeleaf layout

---
+++ thymeleaflayout/src/main/java/com/qi/thymeleaflayout/ThymeleaflayoutApplication.java
@@ -0,0 +1,16 @@
+package com.qi.thymeleaflayout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 参考文章：
+ * https://blog.csdn.net/leon_founder/article/details/82501043
+ * **/
+@SpringBootApplication
+public class ThymeleaflayoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleaflayoutApplication.class, args);
+	}
+
+}
